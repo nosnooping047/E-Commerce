97fcc14b4e67b5c065a08af2e44dbe24f7038494
nosnooping047 <nosnooping047@gmail.com>
2024-03-07 22:53:45 -0300

    versao direct php

--- ProdutosApp/views.py
+++ ProdutosApp/views.py
@@ -3,25 +3,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
 from .models import Produto
 from collections import Counter
-import mercadopago
 
-def criar_preferencia(email_do_usuario, produtos_no_carrinho):
-    sdk = mercadopago.SDK("TEST-5623534421492977-021311-36f84a69d748fe8392e4fab79428ea50-180251525")
-
-    preference_data = {
-        "items": [
-            {
-                "title": "My Item",
-                "quantity": 1,
-                "unit_price": 75.76
-            }
-        ]
-    }
-    preference_response = sdk.preference().create(preference_data)
-    preference = preference_response["response"]
-
-
-    return preference["id"]
 
 def produtos(request):
     if request.method == "POST":
@@ -85,62 +67,157 @@ def carrinho(request):
         produtos_no_carrinho = [(Produto.objects.get(id=int(produto_id)), quantidade) for produto_id, quantidade in cart_counter.items()]
         valor_total = sum(produto.valor_venda * quantidade for produto, quantidade in produtos_no_carrinho)
 
-        email_do_usuario = request.user.email
-        print("Email: views", email_do_usuario)
+        '''email_do_usuario = request.user.email
+        print("Email: views", email_do_usuario)'''
 
         
         if len(produtos_no_carrinho) == 0:
             return redirect('produtos')
         else:
-            preferencia_id = criar_preferencia(email_do_usuario, produtos_no_carrinho)
+            '''preferencia_id = criar_preferencia(email_do_usuario, produtos_no_carrinho)'''
 
-            return render(request, 'carrinho.html', {'preferencia_id': preferencia_id, 'produtos_no_carrinho': produtos_no_carrinho, 'valor_total': valor_total})
+            return render(request, 'carrinho.html', {'produtos_no_carrinho': produtos_no_carrinho, 'valor_total': valor_total})
 
 
-from django.http import JsonResponse
+import json
 import mercadopago
+from django.http import JsonResponse
+
+access_token = "APP_USR-8894352011608591-080301-74476fed4ede9408193e11b38c930d03-180251525"
 
 def process_payment(request):
     if request.method == 'POST':
-        print(request.POST)
-        """try:
-            # Certifique-se de que todos os campos necessários estão presentes no request.POST
-            transaction_amount = float(request.POST.get("transaction_amount"))
-            token = request.POST.get("token")
-            description = request.POST.get("description")
-            installments = int(request.POST.get("installments"))
-            payment_method_id = request.POST.get("payment_method_id")
-            cardholder_email = request.POST.get("cardholderEmail")
-            identification_type = request.POST.get("identificationType")
-            identification_number = request.POST.get("identificationNumber")
-            cardholder_name = request.POST.get("cardholderName")
+        try:
+            sdk = mercadopago.SDK(access_token)
 
-            # Inicialize o SDK do Mercado Pago
-            sdk = mercadopago.SDK("TEST-5623534421492977-021311-36f84a69d748fe8392e4fab79428ea50-180251525")
+            # Decodificar o JSON recebido
+            data = json.loads(request.body)
 
-            # Dados do pagamento
+            # Extrair os dados necessários
+            token = data.get('token')
+            payment_method_id = data.get('payment_method_id')
+            transaction_amount = data.get('transaction_amount')
+            installments = data.get('installments')
+            description = data.get('description')
+            email = data.get('payer', {}).get('email')
+            identificationType = data.get('payer', {}).get('identification', {}).get('type')
+            identificationNumber = data.get('payer', {}).get('identification', {}).get('number')
+            issuer_id = data.get('issuer_id')
+
+            # Preparar os dados do pagamento
             payment_data = {
-                "transaction_amount": transaction_amount,
+                "transaction_amount": float(transaction_amount),
                 "token": token,
                 "description": description,
-                "installments": installments,
+                "installments": int(installments),
                 "payment_method_id": payment_method_id,
                 "payer": {
-                    "email": cardholder_email,
+                    "email": email,
                     "identification": {
-                        "type": identification_type,
-                        "number": identification_number,
-                    },
-                    "first_name": cardholder_name,
+                        "type": identificationType,
+                        "number": identificationNumber
+                    }
                 }
             }
 
-            # Criação do pagamento
+            # Criar o pagamento usando o SDK do Mercado Pago
             payment_response = sdk.payment().create(payment_data)
             payment = payment_response["response"]
+            print(payment)
+            # Retornar uma resposta com os detalhes do pagamento
+            return JsonResponse(payment)
 
-            return JsonResponse({"success": True, "payment": payment})
-        except Exception as e:
-            return JsonResponse({"success": False, "error": str(e)})
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Dados inválidos'}, status=400)
+
     else:
-        return JsonResponse({"success": False, "error": "Método de requisição inválido"})"""
+        return JsonResponse({'error': 'Método não permitido'}, status=405)
+
+'''    
+def process_payment(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        token = data.get('token')
+        payment_method_id = data.get('payment_method_id')
+        transaction_amount = data.get('transaction_amount')
+        installments = data.get('installments')
+        description = data.get('description')
+        email = data.get('payer', {}).get('email')
+        identificationType = data.get('identificationType')
+        identificationNumber = data.get('identificationNumber')
+        issuer_id = data.get('issuer_id')
+
+        payload = {
+            "transaction_amount": 100.5,
+            "token": token,
+            "description": description,
+            "installments": installments,
+            "payment_method_id": payment_method_id,
+            "issuer_id": issuer_id,
+            "payer": {
+                "email": email
+            }
+        }
+        
+        headers = {
+            'accept': 'application/json',
+            'content-type': 'application/json',
+            'Authorization': 'Bearer ' + access_token
+        }           
+
+        
+        print(payload)
+        response = requests.post('https://api.mercadopago.com/v1/payments', json=payload, headers=headers)
+        resultado = response.json()
+
+        print("Result: ", resultado)
+        # Aqui você pode manipular o resultado da API de pagamento.
+        return JsonResponse(resultado)
+
+
+
+def process_payment(request):
+    if request.method == 'POST':
+        result_request = json.loads(request.body.decode('utf-8'))
+        
+        # Supondo que você tenha uma função ou método para obter o access token.
+        access_token = "APP_USR-8894352011608591-080301-74476fed4ede9408193e11b38c930d03-180251525"
+        
+        print(result_request.get('token'))
+        payload = {
+            "transaction_amount": 100.5,
+            "token": result_request.get('token'),
+            "description": result_request.get('description'),
+            "installments": result_request.get('installments'),
+            "payment_method_id": result_request.get('payment_method_id'),
+            "issuer_id": result_request.get('issuer_id'),
+            "payer": {
+                "email": result_request.get('payer').get('email')
+            }
+        }
+
+        headers = {
+            'accept': 'application/json',
+            'content-type': 'application/json',
+            'Authorization': 'Bearer ' + access_token
+        }
+
+        response = requests.post('https://api.mercadopago.com/v1/payments', json=payload, headers=headers)
+        resultado = response.json()
+        
+        # Aqui você pode manipular o resultado da API de pagamento.
+        print("Result: ", resultado)
+
+        # Supondo que você tenha um modelo 'Status' para armazenar o status do pagamento.
+        # status = resultado.get('status')
+        # id_venda = resultado.get('id')
+        # status_obj = Status.objects.create(status=status, id_venda=id_venda)
+
+        # Retornar uma resposta JSON (você pode ajustar conforme necessário).
+        return JsonResponse(resultado)
+
+    else:
+        return JsonResponse({'error': 'Método não permitido'}, status=405)
+
+'''
